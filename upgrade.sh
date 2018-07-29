1f7ac32d1f2abe8d8ab2c7e70fdda18899b5112b
mdrights <linusyangcn@gmail.com>
2018-07-29 12:40:29 +0800

    updated; new features #4

--- upgrade.sh
+++ upgrade.sh
@@ -25,11 +25,19 @@ ret=0
 for dir in a ap d f k l n x xap tcl;
 do
 	for PKG in $(ls ${dir}/*.txz); do
-		pkg=$(cut -d"-" -f1,2 <<< ${PKG##*/})
+		num=$(awk -F'-' '{ print NF }' <<< ${PKG##*/})
+		if [[ $num -eq 4 ]]; then
+			pkg=$(cut -d"-" -f1 <<< ${PKG##*/})
+		else
+			pkg=$(cut -d"-" -f1,2 <<< ${PKG##*/})
+		fi
+
 		if [[ "${APP_EXC[@]}" =~ "$pkg" ]]; then
 			echo "WARN Do not upgrade package: $pkg" | tee -a /tmp/upgrade.log
 			continue
 		fi
+
+		#echo "upgrading $pkg"
 		upgradepkg --install-new $PKG
 		[[ $? -eq 0 ]] && rm $PKG
 	done
@@ -40,7 +48,7 @@ APP_FAIL=$(find ${REPO_DIR}/slackware64/{a,ap,d,f,k,l,n,x,xap,tcl} -name "*.txz"
 
 if [ ! -z "$APP_FAIL" ]; then
 	echo "Finished. You may have one or more packages not installed:"
-	echo $APP_FAIL
+	echo "$APP_FAIL"
 	ret=1
 fi
 
