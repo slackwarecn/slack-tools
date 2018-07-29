4e8c7502adc4a54b9e32be75035f7a34dc568d2f
mdrights <linusyangcn@gmail.com>
2018-07-29 10:50:08 +0800

    updated; new features

--- upgrade.sh
+++ upgrade.sh
@@ -4,30 +4,44 @@
 # Change Log
 # 2017.12.05		set custom directories to be upgraded; make logs.
 # 2018.03.26		add unwant-upgrade package options.
+# 2018.07.29		1. sync remote repo; 2. can prevent selected apps to be installed; 3. delete packages if installation succeeded.
 
 set -eu
 
-# Upgrade these serials, but not e, xap, xfce, kde, t, etc.
-##    Do not upgrade kernel,. Upgrade them manually. 
+# Fill in the apps you do not want to install/upgrade in APP_EXC.
+# Upgrade the series in the `for ... in ...` statement.
 
-REPO_DIR=${REPO_DIR:-/home/user/slackware-repo/slackware64-current/slackware64}
 
-cd $REPO_DIR
+REPO_DIR=${REPO_DIR:-/home/user/slackware-repo/slackware64-current}
+APP_EXC=(mozilla-thunderbird seamonkey seamonkey-solibs)
+
+# rsync first
+rsync -avz --delete linus@192.168.10.108:/mnt/usb1/slackware64-current/ ${REPO_DIR}/
+
+
+cd ${REPO_DIR}/slackware64
 
 ret=0
 for dir in a ap d f k l n x xap tcl;
 do
 	for PKG in $(ls ${dir}/*.txz); do
-		if [[ "$PKG" = *"kernel-generic"* ]] || [[ "$PKG" = *"kernel-huge"* ]] || [[ "$PKG" = *"kernel-modules"* ]];then
-			echo "WARN Do not upgrade official kernel packages ..." | tee -a /tmp/upgrade.log
+		pkg=$(cut -d"-" -f1,2 <<< ${PKG##*/})
+		if [[ "${APP_EXC[@]}" =~ "$pkg" ]]; then
+			echo "WARN Do not upgrade package: $pkg" | tee -a /tmp/upgrade.log
 			continue
 		fi
-		upgradepkg --install-new $PKG || (ret=1; echo "Error while processing: $PKG_" | tee -a /tmp/upgrade.log )
+		upgradepkg --install-new $PKG
+		[[ $? -eq 0 ]] && rm $PKG
 	done
 done
 
-if [[ $ret -ne 0 ]]; then
-	echo "ERROR you have one or more packages wrong."
+
+APP_FAIL=$(find ${REPO_DIR}/slackware64/{a,ap,d,f,k,l,n,x,xap,tcl} -name "*.txz")
+
+if [ ! -z "$APP_FAIL" ]; then
+	echo "Finished. You may have one or more packages not installed:"
+	echo $APP_FAIL
+	ret=1
 fi
 
 exit $ret
